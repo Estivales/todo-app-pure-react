2606cfaf4f6c6e9dccf458150baf085fb2d481e2
Douglas Estivales <douglas.estivales@iphytus.com>
2018-06-29 11:36:19 -0300

    criando primeiro componente app

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,18 +1,18 @@
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extraxt-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: '.app.js'
+        filename: './app.js'
     },
     devServer: {
         port: 8080,
         contentBase: './public',
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],
+        extensions: ['*', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
@@ -24,17 +24,17 @@ module.exports = {
         rules: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
-            exclude: /nodule_modules/,
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            loader: ExtractTextPlugin.extract('css-loader', 'style-loader')
         }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-            loader: 'file'
+            loader: 'file-loader'
         }
     ]
     }
